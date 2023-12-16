45b97bead40401622a60e1a948bad8b014787c55
Ayush Gupta <84263555+AyushGupta93@users.noreply.github.com>
2023-12-16 14:14:48 +0530

    comparision

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,20 @@
+// console.log(2 > 1)
+// console.log(2 < 1)
+// console.log(2 == 1)
+// console.log(2 != 1)
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);  // null converts into 0 then check null == 0 so ans is true
+
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);                   
+
+// ===  it check both the data Types
+
+console.log("2"===2);
