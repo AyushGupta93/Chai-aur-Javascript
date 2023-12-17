5564d8d132fc761ff2b6ddd162030d426311cdd2
Ayush Gupta <84263555+AyushGupta93@users.noreply.github.com>
2023-12-17 17:28:43 +0530

    Array part 1

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,44 @@
+//Arrays
+
+const myArr = [0, 1, 2, 3, 4, 5]
+
+const myHeroes = ["skatimaan", "doga"]
+
+const myArr2 = new Array(1, 2, 3, 5)
+//console.log(myArr2);
+
+//Array Methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop() //removes last ele
+
+// myArr.unshift(6)  // insert ele at start 
+// myArr.shift()    // removes ele at start 
+
+
+// console.log(myArr.includes(7)); // returns boolean type
+// console.log(myArr.indexOf(0));
+
+
+// const newArr = myArr.join()  // converts into string
+
+// console.log(myArr);
+// console.log(newArr);
+
+
+//slice and splice
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1,3) // returns section of an array
+
+console.log(myn1);
+
+console.log("B", myArr);
+
+const myn2 = myArr.splice(1,3)  // changes the original array
+
+console.log(myn2);
+
+console.log("C ", myArr);
