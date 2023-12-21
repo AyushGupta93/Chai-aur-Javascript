fe8fd1b7dd9abeedc15cd27685acafb8292b0561
Ayush Gupta <84263555+AyushGupta93@users.noreply.github.com>
2023-12-21 12:07:44 +0530

    array part 2

--- 02_Basics/01_arrays.js
+++ 02_Basics/01_arrays.js
@@ -1,10 +1,10 @@
 //Arrays
 
-const myArr = [0, 1, 2, 3, 4, 5]
+const myArr = ["a", 1, 2, 3, 4, 5];
 
-const myHeroes = ["skatimaan", "doga"]
+const myHeroes = ["skatimaan", "doga"];
 
-const myArr2 = new Array(1, 2, 3, 5)
+const myArr2 = new Array(1, 2, 3, 5);
 //console.log(myArr2);
 
 //Array Methods
@@ -13,31 +13,28 @@ const myArr2 = new Array(1, 2, 3, 5)
 // myArr.push(7)
 // myArr.pop() //removes last ele
 
-// myArr.unshift(6)  // insert ele at start 
-// myArr.shift()    // removes ele at start 
-
+// myArr.unshift(6)  // insert ele at start
+// myArr.shift()    // removes ele at start
 
 // console.log(myArr.includes(7)); // returns boolean type
-// console.log(myArr.indexOf(0));
-
+console.log(myArr.indexOf("a"));
 
 // const newArr = myArr.join()  // converts into string
 
 // console.log(myArr);
 // console.log(newArr);
 
-
 //slice and splice
 
 console.log("A ", myArr);
 
-const myn1 = myArr.slice(1,3) // returns section of an array
+const myn1 = myArr.slice(1, 3); // returns section of an array
 
 console.log(myn1);
 
 console.log("B", myArr);
 
-const myn2 = myArr.splice(1,3)  // changes the original array
+const myn2 = myArr.splice(1, 3); // changes the original array
 
 console.log(myn2);
 
